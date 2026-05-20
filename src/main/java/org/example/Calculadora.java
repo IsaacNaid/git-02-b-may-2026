8210b7a9f62f8b3a9336ea381aabc1f934ac9b00
Isaac Naid Garcia Benitez <20253ds038@utez.edu.mx>
2026-05-20 13:50:06 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Calculadora {
+    private double num1;
+    private double num2;
+
+    public Calculadora(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+}
